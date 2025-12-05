9f3d17a213b2fc7cc6f5bfdb7b293e3b1f37aa26
Augustin <a.medoatinsa@gmail.com>
2025-12-05 11:18:22 -0500

    0.0.2

--- capacitor-plugin-printer/src/web.ts
+++
@@ -1,9 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-
-import type { PrinterPlugin } from './definitions';
-
-export class PrinterWeb extends WebPlugin implements PrinterPlugin {
-  async print(): Promise<any> {
-    console.log('Not supported web browsers!');
-  }
-}
